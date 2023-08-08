5213ab7cc49712ff90fe18890e94af1aff35bfd6
ecbailen <129152083+ecbailen@users.noreply.github.com>
2023-08-09 01:30:27 +0200

    Add files via upload

---
+++ Saludador.java
@@ -0,0 +1,22 @@
+package pkg02_saludador;
+
+import java.util.Scanner;
+
+public class Saludador {
+
+    /*
+        Pide un nombre por consola y salúdalo por consola.
+    */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("\n");
+        
+        System.out.println("Escribe un nombre");
+        String nombre = sc.next();
+        
+        System.out.println("¡Hola "+nombre+"!");
+        
+    }
+    
+}
